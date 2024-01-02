2b659bd8bc2ad08be7abeaa4174290d42c2f6e93
kavyajadav <kjadhav@stevens.edu>
2024-01-03 05:21:11 +0530

    3rd Jan 2nd

---
+++ WCFWindowsServiceHost/WindowsFormsClient/Form1.cs
@@ -0,0 +1,49 @@
+﻿using MathServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private IMathService nettcpChannel = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nettcpChannel = new ChannelFactory<MathServiceLibrary.IMathService>
+                ("MathService_netTcp").CreateChannel();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+
+            textBox3.Text = nettcpChannel.Adds(obj).ToString();
+        }
+
+        
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MyNumbers obj = new MyNumbers();
+            obj.Number1 = Convert.ToInt32(textBox1.Text);
+            obj.Number2 = Convert.ToInt32(textBox2.Text);
+
+            textBox3.Text = nettcpChannel.Subtracts(obj).ToString();
+        }
+    }
+}
---
+++ WCFWindowsServiceHost/WindowsServiceHost/MyMathService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MathServiceLibrary;
+
+namespace WindowsServiceHost
+{
+    public partial class MyMathService : ServiceBase
+    {
+        private ServiceHost host = null;
+        public MyMathService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(MathService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if(host != null)
+                host.Close();
+            host = null;
+        }
+    }
+}
